3c687bbf7ee17d59d5d2a9ee959d46dfce68638e
vasanth-227 <vasantharaj227@gmail.com>
2025-05-08 10:04:49 +0530

    Use details

---
+++ UseProduct/src/main/java/com/UseProduct/ProductPojo.java
@@ -0,0 +1,69 @@
+package com.UseProduct;
+
+public class ProductPojo {
+	
+	private int id;
+	private String name;
+	private int price;
+	private String color;
+	private int quantity;
+	private int hsncode;
+	private String category;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public int getHsncode() {
+		return hsncode;
+	}
+
+	public void setHsncode(int hsncode) {
+		this.hsncode = hsncode;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+}
---
+++ UseProduct/src/main/java/com/UseProduct/UseProductApplication.java
@@ -0,0 +1,20 @@
+package com.UseProduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class UseProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseProductApplication.class, args);
+	}
+	
+	@Bean
+	
+	 RestTemplate rt() {
+		return new RestTemplate();
+	} 
+}
