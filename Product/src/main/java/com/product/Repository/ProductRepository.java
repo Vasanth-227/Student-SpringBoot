d05238ac4689ace54adf73c2c0ddc806969f5aa6
vasanth-227 <vasantharaj227@gmail.com>
2025-05-08 11:22:33 +0530

    Remove Unnecessary

--- Product/src/main/java/com/product/Repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.product.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.product.Entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-
-}
